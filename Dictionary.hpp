a9be5b9e40e0924e0f8ec4ec0c9626fec29c7ec7
elon-shamir <elon.shamir@cs.huji.ac.il>
2022-06-07 22:32:21 +0300

    init

---
+++ Dictionary.hpp
@@ -0,0 +1,6 @@
+
+
+#ifndef _DICTIONARY_HPP_
+#define _DICTIONARY_HPP_
+
+#endif //_DICTIONARY_HPP_
---
+++ HashMap.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Elon on 07/06/2022.
+//
+
+#ifndef _HASHMAP_HPP_
+#define _HASHMAP_HPP_
+
+class HashMap
+{
+
+};
+
+#endif //_HASHMAP_HPP_
